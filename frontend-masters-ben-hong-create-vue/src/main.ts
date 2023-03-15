99c840a3bcd17552a4c687f49ef6bdbd1842efef
Blake Taylor <sephnescence@gmail.com>
2023-03-15 16:17:43 +1000

    Make note about to use createWebHistory

--- frontend-masters-ben-hong-create-vue/src/main.ts
+++ frontend-masters-ben-hong-create-vue/src/main.ts
@@ -5,6 +5,7 @@ import routes from './components/App/routes'
 
 const router = createRouter({
   // BTTODO - Copilot suggested createWebHistory, but Ben said we won't have time to cover the "HTML5 Mode"
+  //  So apparently, you can run `npm create vue@3` again and there's a prompt to use vue router, which will have createWebHistory set up for you
   //  (The workshops are hard limited to two or three days from memory, so the limitation is
   //    literally because it's truly presented)
   history: createWebHashHistory(),
